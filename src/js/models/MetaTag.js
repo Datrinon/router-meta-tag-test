3dcd4bfe86c05a8fbba62d6b7ee0a4f2c735a206
Datrinon <dtrinh089@gmail.com>
2022-02-10 10:46:40 -0800

    Add MetaTagGroup model
    This model just represents the data used for a group of meta tags.

--- src/js/models/MetaTag.js
+++
@@ -1,5 +0,0 @@
-class MetaTags {
-  constructor() {
-    
-  }
-}
---
+++ src/js/models/MetaTagGroup.js
@@ -0,0 +1,20 @@
+/**
+ * Represents a group of meta tags based on the OpenGraph standard.
+ * 
+ * @param {string} title - The title of the webpage.
+ * @param {string} url - The URL of the webpage, with https:// and www.
+ * @param {string} type - The type of content that website is. See 
+ * [Open Graph Docs](https://ogp.me/#types) for more information.
+ * @param {string} description - A one to two sentence description of the object.
+ * @param {string} image - An image URL to represent the displayed meta tag.
+ * Recommended to be in 2:1 ratio (e.g. 1200 x 630).
+ */
+class MetaTagGroup {
+  constructor(title, url, type, description, image) {
+    this.url = url;
+    this.type = type;
+    this.title = title;
+    this.description = description;
+    this.image = image;
+  }
+}
