8941c8e659d1453b3db89b709fefc9212b2644d5
Datrinon <dtrinh089@gmail.com>
2022-02-10 11:11:28 -0800

    Remove hard-coded meta tags from example pages

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -4,13 +4,6 @@ import { Helmet } from 'react-helmet-async'
 function About() {
   return (
     <>
-    <Helmet>
-      <title>Meta Inspector - About</title>
-      <meta property="og:url" content="URL here" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="page.title / make this customizable!" />
-        <meta property="og:description" content="About page!" />
-    </Helmet>
     <div>This is the about page.</div>
     </>
   )
--- src/js/components/Help.js
+++ src/js/components/Help.js
@@ -4,13 +4,6 @@ import { Helmet } from 'react-helmet-async'
 function Help() {
   return (
     <>
-      <Helmet>
-        <title>Meta Inspector - Help</title>
-        <meta property="og:url" content="URL here" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="page.title / make this customizable!" />
-        <meta property="og:description" content="Help page!" />
-      </Helmet>
       <div>This is the help page.</div>
     </>
   )
