a6d6c4ce1cc7f65814349df5403216e56b3f5538
Datrinon <dtrinh089@gmail.com>
2022-02-10 10:35:46 -0800

    Add MetaTags component
    This should allow us to conditionally customize the MetaTags of each page.

---
+++ src/js/components/Shared/MetaTags.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * Regex which extracts the domain from any given url. Use url.split(GET_DOMAIN_REGEX) and get the capture group at index 1.
+ */
+const GET_DOMAIN_REGEX = new RegExp(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/igm);
+
+/**
+ * Generates a sequence of meta tags based on the OpenGraph standard.
+ * For use inside of a React Helmet element. Based on OpenGraph.xyz's
+ * output of meta tags.
+ * 
+ * See 
+ * https://www.opengraph.xyz/url/https%3A%2F%2Fwww.github.com%2F/ for an example
+ * of Meta Tags in action.
+ * 
+ * @param {string} url - The URL of the site, with https:// and www.
+ * @param {string} type - The type of content that website is. See 
+ * [Open Graph Docs](https://ogp.me/#types) for more information.
+ * @param {string} title - The title of the site.
+ * @param {string} description - A one to two sentence description of the object.
+ * @param {string} image - An image URL to represent the displayed meta tag.
+ * Recommended to be in 2:1 ratio (e.g. 1200 x 630).
+ */
+function MetaTags({ url, type, title, description, image }) {
+
+  return (
+    <>
+      {/* HTML Meta Tags */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* FaceBook Meta Tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content={url.split(GET_DOMAIN_REGEX)[1]} />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description}/>
+      <meta name="twitter:image" content={description}/>
+    </>
+
+  )
+}
+
+export default MetaTags
+
+
+MetaTags.propTypes = {
+  url: PropTypes.string,
+  type: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string
+}
